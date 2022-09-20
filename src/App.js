f5d75711c9eae6c73ae9176ceb977ad84957395e
Joey Corcoran <81182125+joeycor92@users.noreply.github.com>
2022-09-20 16:00:37 +0100

    Done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
+import { API, Storage } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import "@aws-amplify/ui-react/styles.css";
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { createTodo as createTodoMutation, deleteTodo as deleteTodoMutation } from './graphql/mutations';
+import { listTodos } from './graphql/queries';
 
-function App() {
+const initialFormState = { name: '', description: '' }
+
+function App({ signOut }) {
+  const [notes, setTodos] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  async function fetchTodos() {
+    const apiData = await API.graphql({ query: listTodos });
+  const notesFromAPI = apiData.data.listTodos.items;
+  await Promise.all(notesFromAPI.map(async note => {
+    if (note.image) {
+      const image = await Storage.get(note.image);
+      note.image = image;
+    }
+    return note;
+  }))
+  setTodos(apiData.data.listTodos.items);
+  }
+
+  async function createTodo() {
+    if (!formData.name || !formData.description) return;
+  await API.graphql({ query: createTodoMutation, variables: { input: formData } });
+  if (formData.image) {
+    const image = await Storage.get(formData.image);
+    formData.image = image;
+  }
+  setTodos([ ...notes, formData ]);
+  setFormData(initialFormState);
+  }
+
+  async function onChange(e) {
+    if (!e.target.files[0]) return
+    const file = e.target.files[0];
+    setFormData({ ...formData, image: file.name });
+    await Storage.put(file.name, file);
+    fetchTodos();
+  }
+
+  async function deleteTodo({ id }) {
+    const newTodosArray = notes.filter(note => note.id !== id);
+    setTodos(newTodosArray);
+    await API.graphql({ query: deleteTodoMutation, variables: { input: { id } }});
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
+      <h1>My Todos App</h1>
+      <input
+        onChange={e => setFormData({ ...formData, 'name': e.target.value})}
+        placeholder="Todo name"
+        value={formData.name}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'description': e.target.value})}
+        placeholder="Todo description"
+        value={formData.description}
+      />
+      <input
+        type="file"
+        onChange={onChange}
+      />
+      <button onClick={createTodo}>Create Todo</button>
+      <div style={{marginBottom: 30}}>
+      {
+        notes.map(note => (
+          <div key={note.id || note.name}>
+            <h2>{note.name}</h2>
+            <p>{note.description}</p>
+            <button onClick={() => deleteTodo(note)}>Delete note</button>
+            {
+              note.image && <img src={note.image} style={{width: 400}} />
+            }
+          </div>
+        ))
+      }
+      </div>
+      <button onClick={signOut}>Sign Out</button>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
